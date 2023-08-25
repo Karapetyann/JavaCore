82073cd3b227d7bb411421f63fd4d7695ef5174c
Hakob Karapetyan <karapetyan260722@gmail.com>
2023-08-25 22:02:18 +0400

    work1

---
+++ src/classwork/chapter3/Example1.java
@@ -0,0 +1,12 @@
+package classwork.chapter3;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        for (int i = 0; i <= array.length-1 ; i++) {
+            array[i]=i+1;
+            System.out.println(array[i]);
+
+        }
+    }
+}
