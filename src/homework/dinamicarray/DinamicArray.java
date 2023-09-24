3456c09b0e4fabf75b0b24041ccc0c959d23b2fb
Hakob Karapetyan <karapetyan260722@gmail.com>
2023-09-24 16:38:47 +0400

    work1

--- src/homework/dinamicarray/DinamicArray.java
+++ src/homework/dinamicarray/DinamicArray.java
@@ -3,15 +3,14 @@ package homework.dinamicarray;
 public class DinamicArray {
 
     //սա մեր հիմնական մասիվն է, որտեղ պահելու ենք ավելացվող էլեմենտները
-    int n = 10;
-    private int[] array = new int[n];
+    private int[] array = new int[10];
     //սա մեր մասիվի մեջ ավելացված էլեմենտների քանակն է
     private int size = 0;
 
     //ստուգել եթե մասիվի մեջ տեղ չկա-> կանչել extend()
     //և ավելացնենք
     public void push(int value) {
-        if (size == n) {
+        if (size == array.length) {
             extend();
         }
         array[size++] = value;
@@ -21,7 +20,7 @@ public class DinamicArray {
     //2․ քցել հին մասիվի էլեմենտները նորի մեջ
     //3․ հին մասիվի հղումը կապենք նոր մասիվի հղման հետ։
     private void extend() {
-        int[] array1 = new int[n += 10];
+        int[] array1 = new int[array.length + 10];
         for (int j = 0; j < size; j++) {
             array1[j] = array[j];
         }
@@ -32,7 +31,7 @@ public class DinamicArray {
     //եթե տրված ինդեքսը մեր ունեցած մասիվի ինդեքսի սահմաններում է, վերադարձնել
     // մասիվի index-երրորդ էլեմենտը։ Հակառակ դեպքում վերադարձնել -1։
     public int getByIndex(int index) {
-        if (index > n || index < 0) {
+        if (index > size || index < 0) {
             return -1;
         } else {
             return array[index];
